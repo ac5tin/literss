416058afce642887dae27da4f911bf061784097c
austin <austinchang4@gmail.com>
2021-10-15 19:20:34 +0800

    feed base

---
+++ feed/feed.go
@@ -0,0 +1,47 @@
+package feed
+
+import (
+	"fmt"
+	"time"
+
+	uf "github.com/ac5tin/usefulgo"
+)
+
+type Article struct {
+	ID      string
+	Title   string
+	Content string
+	Date    time.Time
+	URL     string
+	FeedID  string
+}
+
+type Feed interface {
+	Fetch(num uint8, t *[]Article) error
+	Get(t *[]Article)
+}
+
+type feedBase struct {
+	ID       string
+	URL      string
+	Name     string
+	Articles []Article
+	Feed
+}
+
+func NewFeedBase(url, name string) feedBase {
+	return feedBase{
+		ID:       uf.GenUUIDV4(),
+		URL:      url,
+		Name:     name,
+		Articles: make([]Article, 0),
+	}
+}
+
+func (f *feedBase) Fetch() error {
+	return fmt.Errorf("Fetch() not implemented")
+}
+
+func (f *feedBase) Get() error {
+	return fmt.Errorf("Get() not implemented")
+}
---
+++ feed/rss.go
@@ -0,0 +1,5 @@
+package feed
+
+type RSS struct {
+	*feedBase
+}
