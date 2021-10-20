f804427eced4ce7d4287d41db55b754abd8e9bc7
austin <austinchang4@gmail.com>
2021-10-20 16:55:31 +0800

    get feed articles api

--- api/handler.go
+++ api/handler.go
@@ -43,3 +43,55 @@ func addFeed(c *fiber.Ctx) error {
 	})
 	return nil
 }
+
+func getFeed(c *fiber.Ctx) error {
+	type inputReq struct {
+		URL      string        `json:"url"`
+		FeedType feed.FeedType `json:"feed_type"`
+		Name     string        `json:"name"`
+	}
+	var input inputReq
+	if err := c.BodyParser(&input); err != nil {
+		c.Status(fiber.ErrBadRequest.Code).JSON(fiber.Map{
+			"success": false,
+			"error":   err.Error(),
+		})
+		return nil
+	}
+
+	articles := new([]feed.Article)
+
+	switch input.FeedType {
+	case feed.FeedTypeRSS:
+		fd, err := feed.FS.GetRSSFeed(input.URL, input.Name)
+		if err != nil {
+			c.Status(fiber.ErrInternalServerError.Code).JSON(fiber.Map{
+				"success": false,
+				"error":   err.Error(),
+			})
+			return nil
+
+		}
+		if err := fd.Get(articles); err != nil {
+			c.Status(fiber.ErrInternalServerError.Code).JSON(fiber.Map{
+				"success": false,
+				"error":   err.Error(),
+			})
+			return nil
+
+		}
+	default:
+		c.Status(fiber.ErrBadRequest.Code).JSON(fiber.Map{
+			"success": false,
+			"error":   "This feed type is currently not supported",
+		})
+		return nil
+	}
+	// success
+	c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success":  true,
+		"articles": articles,
+	})
+
+	return nil
+}
--- api/router.go
+++ api/router.go
@@ -4,4 +4,5 @@ import "github.com/gofiber/fiber/v2"
 
 func Routes(router *fiber.Router) {
 	(*router).Post("/add_feed", addFeed)
+	(*router).Post("/get_feed", getFeed)
 }
